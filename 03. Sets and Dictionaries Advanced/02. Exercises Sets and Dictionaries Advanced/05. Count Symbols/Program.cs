495306a4467cfec91f1e4acfd376ddf513f0b028
Iliyana Dzhabirova <il.dzhabirova@abv.bg>
2024-01-26 18:40:59 +0200

    add files

---
+++ 03. Sets and Dictionaries Advanced/02. Exercises Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] input = Console.ReadLine().ToCharArray();
+SortedDictionary<char, int> charCounter = new SortedDictionary<char, int>();
+
+foreach (char c in input)
+{
+    if (charCounter.ContainsKey(c))
+    {
+        charCounter[c]++;
+    }
+    else
+    {
+        charCounter.Add(c, 1);
+    }
+}
+foreach (char c in charCounter.Keys)
+{
+    Console.WriteLine($"{c}: {charCounter[c]} time/s"); 
+}
