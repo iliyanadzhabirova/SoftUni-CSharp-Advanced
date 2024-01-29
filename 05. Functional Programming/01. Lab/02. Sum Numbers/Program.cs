170f0fec573b1ef1d7db22b60774fa7cc7388a5c
Iliyana Dzhabirova <il.dzhabirova@abv.bg>
2024-01-29 17:02:58 +0200

    .

---
+++ 05. Functional Programming/01. Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] numbers = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+Console.WriteLine(numbers.Length);
+Console.WriteLine(numbers.Sum());
