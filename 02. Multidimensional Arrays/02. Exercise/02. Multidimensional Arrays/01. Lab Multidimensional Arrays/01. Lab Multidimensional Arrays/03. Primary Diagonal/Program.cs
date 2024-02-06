1b3910e925b06229d8e036c3dc93affbb75d407c
Iliyana Dzhabirova <il.dzhabirova@abv.bg>
2024-02-06 19:04:14 +0200

    Upload CSharp Advanced Exercises

---
+++ 02. Multidimensional Arrays/02. Exercise/02. Multidimensional Arrays/01. Lab Multidimensional Arrays/01. Lab Multidimensional Arrays/03. Primary Diagonal/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] numbers = Console.ReadLine()
+                           .Split(" ")
+                           .Select(int.Parse)
+                           .ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = numbers[col];
+    }
+}
+
+int diagonalSum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    diagonalSum += matrix[i, i];
+}
+
+Console.WriteLine(diagonalSum);
---
+++ 02. Multidimensional Arrays/02. Exercise/02. Multidimensional Arrays/02. Exercise Multidimensional Arrays/01. Diagonal Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿int size =int.Parse(Console.ReadLine());
+
+int sumDiagonalOne = 0;
+int sumDiagonalTwo = 0;
+
+int[,] matrix = new int[size, size];
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] newRow = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = newRow[col];
+        if (row == col)
+        {
+            sumDiagonalOne += matrix[row, col];
+        }
+    }
+}
+for (int row = 0; row < size; row++)
+{
+    sumDiagonalTwo += matrix[row, size-1-row];
+}
+Console.WriteLine(Math.Abs(sumDiagonalTwo-sumDiagonalOne));
